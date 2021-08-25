16f0dc34faf60ab78244d7d00b7f13dbf9da8ced
Akudbilla Daniel <akuddev@gmail.com>
2021-08-25 14:38:46 +0000

    added tailFactorial test

---
+++ tests/tailFactorial.test.js
@@ -0,0 +1,7 @@
+import { tailFactorial } from "../tailFactorial";
+
+describe("tailFactorial", () => {
+  test("should return the factorial of a number ", () => {
+    expect(tailFactorial(3)).toBe(6);
+  });
+});
