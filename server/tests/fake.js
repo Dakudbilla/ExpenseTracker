7977475730fe2ff57a559998ecbfbcf6fefa7370
Akudbilla Daniel <akuddev@gmail.com>
2021-08-26 23:40:23 +0000

    finalised server tests

--- server/tests/fake.js
+++ server/tests/fake.js
@@ -2,21 +2,3 @@ exports.fakeTransaction = () => ({
   text: "Eat",
   amount: 5,
 });
-
-exports.fakeManyTransactions = () => [
-  {
-    _id: "10",
-    text: "Drink",
-    amount: -8,
-  },
-  {
-    _id: "11",
-    text: "Salary",
-    amount: 900,
-  },
-  {
-    _id: "12",
-    text: "Birthday",
-    amount: 600,
-  },
-];
